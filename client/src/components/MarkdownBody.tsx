65eed2762676448989f50e96d0c726326290adeb
michaelt-rgb <michael.t@tastewise.io>
2026-05-26 15:51:50 +0300

    chore: remove server/, client/, Docker files (migrated to Electron)

--- client/src/components/MarkdownBody.tsx
+++
@@ -1,9 +0,0 @@
-export default function MarkdownBody({ content }: { content: string }) {
-  return (
-    <div className="bg-gray-800/30 rounded-lg p-6 overflow-x-auto">
-      <pre className="text-sm text-gray-300 whitespace-pre-wrap font-mono leading-relaxed">
-        {content}
-      </pre>
-    </div>
-  );
-}
